f3fb1a1e362ec96b79e3b00cefadf666a988dddc
VANGALA SHARON <153933815+URK23CS1204@users.noreply.github.com>
2026-07-23 15:27:25 +0530

    Create Evenno.java

---
+++ Day1/Evenno.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class evenNo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		//Even Number
+		
+//		for(int i = 2;i<n;i +=2) {
+//			System.out.println(i);
+//		}
+		
+		//odd Number
+		for(int i = 1;i<n;i +=2) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
