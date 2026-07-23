bf02300ead638ff50892d8c9eb22327db861cb55
VANGALA SHARON <153933815+URK23CS1204@users.noreply.github.com>
2026-07-23 15:24:48 +0530

    Create AddThreeNumbers.java

---
+++ Day1/AddThreeNumbers.java
@@ -0,0 +1,15 @@
+package FirstJavaProgram;
+
+public class AddThreeNumbers {
+
+	public static void main(String[] args) {
+		
+		int a = 5;
+		int b = 10;
+		int c = 15;
+		int sum = a + b + c;
+		System.out.println(sum);
+
+	}
+
+}
